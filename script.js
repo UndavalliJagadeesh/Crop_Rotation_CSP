3f33c6cae455dc1aef145c89b05d976190c7f53e
JAGADEESH UNDAVALLI <84311163+UndavalliJagadeesh@users.noreply.github.com>
2023-06-08 18:36:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
 // Example function to calculate crop rotation schedule
 var cropList=[]
 function calculateSchedule() {
